dce621acd256783036a26a2ab149bd32e746d25d
Joe <josephinverso@gmail.com>
2025-02-05 15:18:42 -0500

    Wed Class

---
+++ Assets/Scripts/LivesController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LivesController : MonoBehaviour
+{
+    public TMP_Text livesTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesTxt.SetText("Lives: " + GameManager.instance.GetLives());  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
